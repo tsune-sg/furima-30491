1eee3c3919616ca93ab928612c00f28ba0f75b6d
tsune-sg <71683011+tsune-sg@users.noreply.github.com>
2020-10-30 17:33:48 +0900

    rubocop実行

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,13 +1,12 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:edit, :show, :update,:destroy]
+  before_action :set_item, only: [:edit, :show, :update, :destroy]
   def index
     @items = Item.order('created_at DESC')
   end
 
   def show
   end
-
 
   def destroy
     if current_user == @item.user && @item.destroy
@@ -16,7 +15,6 @@ class ItemsController < ApplicationController
       render :show
     end
   end
-
 
   def new
     @item = Item.new
@@ -34,7 +32,7 @@ class ItemsController < ApplicationController
 
   def edit
     unless current_user == @item.user
-      redirect_to root_path
+    redirect_to root_path
     end
   end
 
