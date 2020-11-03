43f93d09dfe8dee626eb4f89fe7fc7fa463408bb
tsune-sg <71683011+tsune-sg@users.noreply.github.com>
2020-11-03 18:47:29 +0900

    Revert "credit実装"
    This reverts commit a2352e6e6add3e2205f481cbd57389f22d9fb72d.

--- app/models/purchase.rb
+++
@@ -1,5 +0,0 @@
-class Purchase < ApplicationRecord
-  attr_accessor :token
-  validates :price, presence: true
-  validates :token, presence: true
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_03_071418) do
+ActiveRecord::Schema.define(version: 2020_10_22_225850) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "name", null: false
@@ -46,12 +46,6 @@ ActiveRecord::Schema.define(version: 2020_11_03_071418) do
     t.integer "price", null: false
     t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_items_on_user_id"
-  end
-
-  create_table "purchases", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
-    t.integer "price", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
