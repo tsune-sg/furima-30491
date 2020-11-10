773f236a462803d09861fc0d2551decd311a2fd9
tsune-sg <71683011+tsune-sg@users.noreply.github.com>
2020-11-10 10:36:45 +0900

    Update User.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i.freeze
-  validates :password, format: { with: VALID_PASSWORD_REGEX }
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]{6,100}+\z/i.freeze
+  validates :password, presence: true, length: { minimum: 6, maximum: 100}, format: { with: VALID_PASSWORD_REGEX}
   validates :nickname, presence: true
   validates :birth, presence: true
   validates :firstname, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
