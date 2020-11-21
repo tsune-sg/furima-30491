5383fa1acb31b3992978cc9cfb23e234084bfac1
tsune-sg <71683011+tsune-sg@users.noreply.github.com>
2020-11-21 19:26:09 +0900

    削除後復元

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -11,12 +11,11 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    #if current_user == @item.user && 
-    @item.destroy
+    if current_user == @item.user && @item.destroy
       redirect_to root_path
-    #else
-      #render :show
-    #end
+    else
+      render :show
+    end
   end
 
   def new
